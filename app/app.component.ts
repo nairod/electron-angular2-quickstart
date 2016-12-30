4b8bb786e9d0c066c5672154f646788e8daaad2d
Dorian Nyffeler <dorian.nyffeler@bison-group.com>
2016-12-30 21:57:11 +0100

    bump versions

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-<h1>My First Angular 2 App</h1>
+<h1>My First Angular 24 App</h1>
 <p>Put your app template here. Any changes to this template, or any other file in the \`app\` folder will be automagically
 picked up by the BrowserSync live reload mechanism.</p>
 `})
